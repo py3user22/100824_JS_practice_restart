a77c60f0db012c32f6b502fd3d474cf01d1aee36
Jesus G <113757515+py3user22@users.noreply.github.com>
2024-10-08 20:26:13 -0500

    1st commit and save of demo1

---
+++ 1008_js_practice_restart.js
@@ -0,0 +1,9 @@
+// this is a comment, single-lined --> console.log() used to print to console
+
+console.log('Today is good to restart my JS learning!')
+
+/*
+this is a multi-lined comment used in JS
+*/ 
+
+
